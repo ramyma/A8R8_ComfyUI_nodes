dd960735614e9033518aaa58a572082307cd3836
ramyma <ramy.mahmoudi@gmail.com>
2024-05-12 04:06:07 +0300

    Add batch support and refactor

--- nodes.py
+++ nodes.py
@@ -13,20 +13,17 @@ class Base64ImageInput:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "bas64_image": ("STRING", {
-                    "multiline": False,
-                    "default": ""
-                }),
+                "bas64_image": ("STRING", {"multiline": False, "default": ""}),
             },
         }
 
     RETURN_TYPES = ("IMAGE",)
 
-    FUNCTION = "test"
+    FUNCTION = "process_input"
 
     CATEGORY = "A8R8"
 
-    def test(self, bas64_image):
+    def process_input(self, bas64_image):
         if bas64_image:
             image_bytes = base64.b64decode(bas64_image)
 
@@ -40,47 +37,48 @@ class Base64ImageInput:
 
 
 class Base64ImageOutput:
-
     def __init__(self):
         pass
 
     @classmethod
     def INPUT_TYPES(s):
-        return {"required":
-                {"images": ("IMAGE", ), },
-                }
+        return {
+            "required": {
+                "images": ("IMAGE",),
+            },
+        }
 
     RETURN_TYPES = ()
 
-    FUNCTION = "test"
+    FUNCTION = "process_output"
 
     OUTPUT_NODE = True
 
     CATEGORY = "A8R8"
 
-    def test(self, images: list[torch.Tensor]):
-        image = images[0]
-        i = 255. * image.cpu().numpy()
+    def process_image(self, image):
+        i = 255.0 * image.cpu().numpy()
         img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
 
         buffered = io.BytesIO()
-        img.save(buffered, optimize=False,
-                 format='png', compress_level=4)
+        img.save(buffered, optimize=False, format="png", compress_level=4)
 
         base64_image = base64.b64encode(buffered.getvalue()).decode()
+        return base64_image
 
-        return {"ui": {"images": [base64_image]}}
+    def process_output(self, images: list[torch.Tensor]):
+        return {"ui": {"images": [self.process_image(image) for image in images]}}
 
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     "Base64ImageInput": Base64ImageInput,
-    "Base64ImageOutput": Base64ImageOutput
+    "Base64ImageOutput": Base64ImageOutput,
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Base64ImageInput": "Base64Image Input Node",
-    "Base64ImageOutput": "Base64Image Output Node"
+    "Base64ImageOutput": "Base64Image Output Node",
 }
